93e0ee5b6fc9a7070d57b5bd36a2b23d741489e1
Origami-B <972397381@qq.com>
2023-07-03 21:10:45 +0800

    Add definition of device functions

---
+++ efs/efs_device.c
@@ -0,0 +1,15 @@
+#include "headers.h"
+
+//TODO
+eris_device_t eris_device_find(const char *name){
+
+}
+
+
+eris_err_t  eris_device_open (eris_device_t dev, uint16_t oflag){
+
+}
+
+eris_err_t  eris_device_close(eris_device_t dev){
+
+}
